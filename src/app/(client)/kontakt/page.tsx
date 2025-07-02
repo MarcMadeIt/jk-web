ff87ace8173476796d36928fba15ad3bbf1ce1e4
Marc Made It <spurdo.dk@gmail.com>
2025-07-02 19:39:43 +0200

    Update page.tsx

--- src/app/(client)/kontakt/page.tsx
+++ src/app/(client)/kontakt/page.tsx
@@ -4,7 +4,7 @@ import ContactPage from "./ContactPage";
 export const metadata: Metadata = {
   title: "Contact",
   description:
-    "Ready to start your project? Contact Arzonic to discuss your goals and find the right digital solution.",
+    "Ready to start your project. Contact Arzonic to discuss your goals and find the right digital solution.",
 };
 
 export default function Page() {
